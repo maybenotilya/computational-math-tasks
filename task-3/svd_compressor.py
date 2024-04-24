93744002748c1c117d7872572a1ab9ad78b7e325
Ilya Syresenkov <ilya.syresenkov.04@gmail.com>
2024-04-24 03:19:13 +0300

    task 3: cli

--- task-3/svd_compressor.py
+++ task-3/svd_compressor.py
@@ -1,11 +1,19 @@
 from argparser import parse_args
 from svd.NumpySVD import NumpySVD
 from compressor.Compressor import Compressor
-from pathlib import Path
-
 
 if __name__ == '__main__':
-    svd = NumpySVD()
+    args = parse_args()
     compressor = Compressor()
-    compressor.compress(Path('basov.jpeg'), Path('temp.penis'), svd, 1)
-    compressor.decompress(Path('temp.penis'), Path('babasov.png'))
+    if args.mode == "decompress":
+        compressor.decompress(args.in_file, args.out_file)
+    elif args.mode == "compress":
+        if args.method == "numpy":
+            svd = NumpySVD()
+        else:
+            print("Wrong method is given")
+            exit(1)
+        compressor.compress(args.in_file, args.out_file, svd, args.compression)
+    else:
+        print("Wrong mode is given.")
+        exit(1)
